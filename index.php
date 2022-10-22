8b1d405a1fad1554db1f483d2eee2271de73cc31
Ian Yang <iyang30@gatech.edu>
2022-10-22 03:28:56 -0400

    Starting some php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>red flag?</title>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <script src="scripts/content.js"></script>
+
+        
+</head>
+<body>
+    <header id="header">
+        <h1 class="text-center">red flag?</h1>
+        <h3 class="text-center" id="status"></h3>
+    </header>
+
+    <div class="container mt-3" style="height: 420px; width: 400px;" id="header">
+        <h4 class="text-center">this company receives a:</h4>
+        <h4 class="text-center">similar sites that we approve:</h4>
+
+        </PHP 
+        echo shell_exec("python test.py 'parameter1'");
+        ?>
+
+    </div>
+</body>
+</html>
