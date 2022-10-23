3b31ae24297a32e9e61781d8f3a85b7e5c32c744
Ian Yang <iyang30@gatech.edu>
2022-10-22 22:08:22 -0400

    Changes

--- scripts/content.js
+++ scripts/content.js
@@ -42,84 +42,55 @@ function getCurrentTabUrl(callback) {
 
   function checkForEthics(siteUrl) {
     if (siteUrl in websites_to_check) {
-      document.getElementById('heres-why').textContent = "here's why:"
+      var redflag = document.getElementById('the-green-flag');
+      redflag.parentNode.removeChild(redflag);
+      document.getElementById('heres-why').textContent = "here's why:";
       for (let i = 0; i < 4; i++) {
-        //var tbodyRef = document.getElementById('bad-reasons').getElementsByTagName('tbody')[0];
-
-        // Insert a row at the end of table
-        //var newRow = tbodyRef.insertRow();
-
-        // Insert a cell at the end of the row
-        //var newCell = newRow.insertCell();
-
-        // Append a text node to the cell
-        var newText = document.createTextNode(websites_to_check[siteUrl][0][i]);
-        //newCell.appendChild(newText);
         document.getElementById('heres-why').textContent += ('\n' + websites_to_check[siteUrl][0][i]);
       }
       document.getElementById('recommendations').textContent = websites_to_check[siteUrl][1];
       document.getElementById('recommendations').style.fontStyle = "italic";
-      var redflag = document.getElementById('the-green-flag');
-      redflag.parentNode.removeChild(redflag);
     } else {
+      var greenflag = document.getElementById('the-red-flag');
+      greenflag.parentNode.removeChild(greenflag);
       document.getElementById('ethics').textContent = "approved!";
       document.getElementById('ethics').style.fontStyle = "italic";
       var theBar = document.getElementById('bottom-row');
       theBar.parentNode.removeChild(theBar);
       var remove = document.getElementById('remove_or_not');
       remove.parentNode.removeChild(remove);
-      var greenflag = document.getElementById('the-red-flag');
-      greenflag.parentNode.removeChild(greenflag);
     }
     
   }
-  
-/*chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-   function(tabs){
-      alert(tabs[0].url);
-   }
-);
-(() => {
-    let currentSite = "";
-
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, value, siteId } = obj;
-
-        if (type === "NEW") {
-            currentSite = siteId;
-            newSiteLoaded();
-        }
-    });
-})*/
 
 const websites_to_check = {
   "monsanto": [],
   "nestle": [["slave labor", "child labor", "harms environment", "water thieves"], ["Earth's Best Organic"]],
   "bp": [["unsafe workspace", "harms environment"],["Z energy"]],
-  "facebook": [["electricity usage", "tax avoidance", "real-name user requirement policies", "censorship policies", "mishandling of user data"],["Reddit"]],
+  "facebook": [["electricity usage", "tax avoidance", "censorship policies", "mishandling of user data"],["Reddit"]],
   "bayer": [["false advertisement"], []],
-  "amazon": [["misleading reviews", "poor working conditions", "product scams", "tax avoidance", "worker exploitation", "animal testing", "harms environment", "controversial technologies"],[]],
-  "db": [],
-  "hsbc": [],
-  "coca-cola": [],
-  "google": [],
-  "costco": [],
+  "amazon": [["misleading reviews", "poor working conditions", "product scams", "tax avoidance", "worker exploitation", "animal testing", "harms environment", "controversial technologies"],["Better World Books"]],
+  "db": [["falsified records", "tax avoidance", "money laundering"],[]],
+  "hsbc": [["indigenous rights violations", "money laundering", "harms environment"],[]],
+  "coca-cola": [["harms environment", "racial discrimination", "animal cruelty", "anti-abortion donations"],["Aura Bora"]],
+  "google": [["tax avoidance", "mishandling of user data", "privacy violations", "censorship policies", "lax advertising content controls"],["Ecosia"]],
+  "costco": [["pressured and humiiated workers", "animal cruelty", "chicken's inhumane treatment"],["Aldi"]],
   "popeyes": [],
   "chickfila": [],
-  "apple": [],
-  "shein": [],
-  "nike": [],
-  "tencent": [],
-  "activision": [],
-  "comcast": [],
-  "peta": [],
-  "cargill": [],
+  "apple": [["extreme working conditions", "use of deadly chemicals", "child-labour abuses"],["Fairphone"]],
+  "shein": [[],["Kotn"]],
+  "nike": [[],["Hylo Aesthetics"]],
+  "tencent": [[],["Good Eggs"]],
+  "activision": [["discriminated female workers", "psychological tormentation of female workers"],["Nintendo"]],
+  "comcast": [[],["Liberty Global"]],
+  "peta": [["sexist", "promote euthanasia"],["Animal Liberation Front"]],
+  "cargill": [["contaminated meat"],["Mosa Meat"]],
   "koch": [],
   "conagra": [],
   "conrteva": [],
   "pfizer": [],
-  "walmart": [],
-  "twitter": [],
+  "walmart": [["hurts local communities", "foreign and child labor", "discrimination against women and workers with dissabilities"],["Trader Joe's"]],
+  "twitter": [[],["Reddit"]],
   "disney": [],
   "att": [],
   "jnj": [],
@@ -132,7 +103,7 @@ const websites_to_check = {
   "victoriassecret": [],
   "mango": [],
   "urbanoutfitters": [],
-  "primark": [],
+  "primark": [[],["girlfriend collective"]],
   "missguided": [],
   "fashionnova": [],
   "americaneagle": [],
@@ -143,18 +114,14 @@ const websites_to_check = {
   "cos": [],
   "dillards": [],
   "familydollar": [],
-  "childrensplace": [],
+  "childrensplace": [["bellabu bear"]],
   "regmovies": [],
   "mcdonalds": [],
   "belk": [],
   "sears": [],
   "kroger": [],
-  "hollister": [],
-  "gap": [],
+  "hollister": [["body shamming", "negative body images", "promote a toxic beauty standard"],["ADIDAS"]],
+  "gap": [["poor working conditions", "child labor", "worker exploitation"],["VETTA"]],
   "cvs": [["false advertisement", "lack of proper research", "worker exploitation"],[]],
   "kmart": [["worker exploitation", "child labor", "poor working conditions"],[]]
 }
-
-const websites_to_recommend =[
-  "abercrombie"
-]
